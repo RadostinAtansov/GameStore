91f6e9ddda22275dcbe1de0591ab359513b03c9c
Radostin <radstatan@gmail.com>
2023-07-30 23:25:54 +0300

    integrate slick slider

---
+++ GameStore/wwwroot/js/custom.js
@@ -0,0 +1,43 @@
+﻿
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    
+    //responsive: [
+    //    {
+    //        breakpoint: 768,
+    //        settings: {
+    //            arrows: false,
+    //            centerMode: true,
+    //            centerPadding: '40px',
+    //            slidesToShow: 3
+    //        }
+    //    },
+    //    {
+    //        breakpoint: 480,
+    //        settings: {
+    //            arrows: false,
+    //            centerMode: true,
+    //            centerPadding: '40px',
+    //            slidesToShow: 1
+    //        }
+    //    }
+    //]
+});
+
+//$('.multiple-items').slick({
+//   centerMode: true,
+//   centerPadding: '0',
+//   slidesToShow: 3,
+//   arrows: true,
+//   centerMode:true,
+//   infinite: true,
+//   slidesToScroll: 1,
+//    });
+//};
